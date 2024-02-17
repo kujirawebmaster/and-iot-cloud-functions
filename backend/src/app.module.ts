8036fb38bf75a755f900d57a453042ee58ca8caa
nakaga77777 <nakaga77777@gmail.com>
2024-02-17 14:52:01 +0900

    Modify DMS-747

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BaycomController } from './baycom.controller';
 import { KeyvoxController } from './keyvox.controller';
+import { PaymentByPayPayController } from './payPay.controller';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { KeyvoxController } from './keyvox.controller';
     AppController,
     BaycomController,
     KeyvoxController,
+    PaymentByPayPayController,
   ],
   providers: [AppService],
 })
